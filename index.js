6f947c87cdc5e8be91560a0e990730d9aea8af37
Vazgen Barfyan <vbarfyan@gmail.com>
2021-03-19 18:43:38 +0400

    ip

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const puppeteer = require("puppeteer");
+const http = require('http');
 
 async function main() {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox"]
-  });
-  const tab = await browser.newPage();
-  const text = await (await tab.goto("http://example.com/")).text();
-  console.log(text);
-  console.log("done");
-  browser.close();
+  // const browser = await puppeteer.launch({
+  //   headless: true,
+  //   args: ["--no-sandbox"]
+  // });
+  // const tab = await browser.newPage();
+  // const text = await (await tab.goto("http://example.com/")).text();
+  // console.log(text);
+  // console.log("done");
+  // browser.close();
+
+
+
+  http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+          resp.on('data', function(ip) {
+              console.log(ip.toString());
+          });
+          resp.on('error', (err)=>{
+            console.log(err);
+          });
+      });
 }
 
 

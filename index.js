1a66570593bb1052cc311c9b51770f78f0d3ad57
Vazgen Barfyan <vbarfyan@gmail.com>
2021-03-19 18:18:15 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const puppeteer = require("puppeteer");
+
+async function main() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"]
+  });
+  const tab = await browser.newPage();
+  const text = await (await tab.goto("http://example.com/")).text();
+  console.log(text);
+  console.log("done");
+  browser.close();
+}
+
+main();
